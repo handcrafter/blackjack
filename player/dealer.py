e925c7bc1a68af2a91fc14f22c3c0cfe62d2d983
Jaejun Lee <ljj7975@gmail.com>
2021-02-28 20:43:16 -0500

    updated dealer

--- player/dealer.py
+++ player/dealer.py
@@ -15,8 +15,27 @@ class Dealer(Player):
         super().__init__(name)
 
     def bet(self) -> int:
-        raise NotImplementedError
+        """Dealer cannot bet
+
+        Raises:
+            NotImplementedError: Dealer should not bet
+
+        Returns:
+            int: invalid output
+        """
+        raise NotImplementedError("Dealer should not be betting")
 
     def act(self,
             hand: List[Card]) -> Action:
+        """Dealer must be hitting when the hand is less than 16
+
+        Args:
+            hand (List[Card]): [description]
+
+        Raises:
+            NotImplementedError: [description]
+
+        Returns:
+            Action: [description]
+        """
         raise NotImplementedError
